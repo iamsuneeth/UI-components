4abd36c3149e3128eb462447337b79f7306d91a1
iamsuneeth <suneeth.lenin@gmail.com>
2017-05-20 00:28:34 +0530

    final commit - added animations

--- help/help.js
+++ help/help.js
@@ -1,3 +1,12 @@
 $(function(){
 
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    var self=this;
+    $('.tab-pane').toggleClass('no-shadow');
+    $( ".nav-tabs" ).slideToggle("fast");
+    $( ".nav-tabs" ).slideToggle("slow",function(){
+        $('.tab-pane').toggleClass('no-shadow');
+    });
+    
+});
 });
